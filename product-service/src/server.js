5ce0ce2ca2669ed2bec59418bc1ddf0529776cf4
yevin04 <yevinraja@gmail.com>
2026-02-04 19:23:11 +0530

    cors update

--- product-service/src/server.js
+++ product-service/src/server.js
@@ -10,11 +10,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-// app.use(cors({
-//   origin: '*',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
