be289445ce05d93c5a1c66ddcfd2b711520c0786
yevin04 <yevinraja@gmail.com>
2026-02-05 16:31:17 +0530

    Fix Express 5 crash by removing incompatible wildcard route

--- product-service/src/server.js
+++ product-service/src/server.js
@@ -18,8 +18,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-// Important for ALB preflight
-app.options("*", cors());
+
 
 app.use(express.json());
 
